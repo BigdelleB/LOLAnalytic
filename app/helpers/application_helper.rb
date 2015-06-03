8a51feff3457a1bde9ef3d5a1ad1d34ad575ef55
Brian Bigdelle <bb586@cornell.edu>
2015-06-03 17:05:29 -0400

    wrote method in application controller helper to get JSONS from the login name and region provided, currently just displays entire JSON on screen

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+	require 'net/http'
+	require 'open-uri'
+
+	def extractJSON(login)
+		username= login.Username
+		region= login.Region
+		url="https://"+region+".api.pvp.net/api/lol/"+region+"/v1.4/summoner/by-name/" + username+"?api_key=28ba1d65-cda8-4e79-90ad-aad6b1ab6326"
+		#url=URI.parse(URI.encode(url.strip))
+		uri=URI.parse(url)
+		uri=uri.open
+		str=uri.read
+		return str
+
+
+
+		#result= Net::HTTP.get(URI.parse(url))
+		#result= RestClient.get(result)
+		
+		#puts result;
+	end
 end
