1c687c8eff56f840513ee5597c9fe8c7c93b6940
Brian Bigdelle <bb586@cornell.edu>
2015-06-05 21:22:47 -0400

    created new page for stats with a data_controller class and some new views, now to figure out how to pass the particular login info to the new page and then use the riot API to get information

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,18 +1,4 @@
 class DataController < ApplicationController
-	before_action :set_login, only: [:show, :edit, :update, :destroy]
-
-  # GET /logins
-  # GET /logins.json
   def index
-    @logins = Login.all
   end
-	
-
-	def show_kills
-	end
-
-
-
-
-
 end
---
+++ app/helpers/data_helper.rb
@@ -0,0 +1,28 @@
+module DataHelper
+
+	require 'net/http'
+	require 'open-uri'
+
+	def extractJSON(login)
+		username= login.Username
+		region= login.Region
+		url="https://"+region+".api.pvp.net/api/lol/"+region+"/v1.4/summoner/by-name/" + username+"?api_key=28ba1d65-cda8-4e79-90ad-aad6b1ab6326"
+		#url=URI.parse(URI.encode(url.strip))
+		uri=URI.parse(url)
+		uri=uri.open
+		str=uri.read
+		return str
+	end
+
+	def summoner_id(login)
+		champion= extractJSON(login)
+		
+		#get champion id 
+		#return the id to use for future methods
+	end 
+
+	def summoner_stats(login)
+		#parse JSON to get the kills and match history stuff from it
+		#return all the necessary values in a table
+	end
+end
