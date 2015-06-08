97b9dd7f700b01f82c66b02b142d4bf701ca7a11
Brian Bigdelle <bb586@cornell.edu>
2015-06-08 09:58:43 -0400

    apparently didnt save last commit so had to redo everything :'(

--- app/helpers/data_helper.rb
+++ app/helpers/data_helper.rb
@@ -3,9 +3,10 @@ module DataHelper
 	require 'net/http'
 	require 'open-uri'
 
+
 	def extractJSON(login)
-		username= login.Username
-		region= login.Region
+		username= login.fetch(:Username)
+		region= login.fetch(:Region)
 		url="https://"+region+".api.pvp.net/api/lol/"+region+"/v1.4/summoner/by-name/" + username+"?api_key=28ba1d65-cda8-4e79-90ad-aad6b1ab6326"
 		#url=URI.parse(URI.encode(url.strip))
 		uri=URI.parse(url)
