81f859a2c429c2d27f407ca6861aae2f3cbbd5d8
Brian Bigdelle <bb586@cornell.edu>
2015-06-08 10:35:13 -0400

    helper methods, instance fields and more methods

--- app/helpers/data_helper.rb
+++ app/helpers/data_helper.rb
@@ -3,27 +3,33 @@ module DataHelper
 	require 'net/http'
 	require 'open-uri'
 
+	@@champion_name
+	@@champ_id
 
+
+	#gets the JSON values I need from a champion to start calling more advanced methods
 	def extractJSON(login)
 		username= login.fetch(:Username)
+		@@champion_name=username
 		region= login.fetch(:Region)
 		url="https://"+region+".api.pvp.net/api/lol/"+region+"/v1.4/summoner/by-name/" + username+"?api_key=28ba1d65-cda8-4e79-90ad-aad6b1ab6326"
-		#url=URI.parse(URI.encode(url.strip))
 		uri=URI.parse(url)
 		uri=uri.open
 		str=uri.read
 		return str
 	end
 
+	#gets the summoner_id
 	def summoner_id(login)
 		champion= extractJSON(login)
+		id = JSON.parse(champion)['chelski']['id']
+		@@champ_id=id
+		return id
 		
-		#get champion id 
-		#return the id to use for future methods
 	end 
 
-	def summoner_stats(login)
-		#parse JSON to get the kills and match history stuff from it
-		#return all the necessary values in a table
+
+	def summoner_avg_kills_per_game(login)
+		
 	end
 end
