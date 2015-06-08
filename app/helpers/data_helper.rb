c1eb02967107403072ef513c5452a8100f10e1f2
Brian Bigdelle <bb586@cornell.edu>
2015-06-08 12:08:25 -0400

    ranked_stats_JSON method written to get a huge list of ranked stats by champion played for a summoner

--- app/helpers/data_helper.rb
+++ app/helpers/data_helper.rb
@@ -5,6 +5,7 @@ module DataHelper
 
 	@@champion_name
 	@@champ_id
+	@@region
 
 
 	#gets the JSON values I need from a champion to start calling more advanced methods
@@ -12,24 +13,31 @@ module DataHelper
 		username= login.fetch(:Username)
 		@@champion_name=username
 		region= login.fetch(:Region)
+		@@region=region
 		url="https://"+region+".api.pvp.net/api/lol/"+region+"/v1.4/summoner/by-name/" + username+"?api_key=28ba1d65-cda8-4e79-90ad-aad6b1ab6326"
 		uri=URI.parse(url)
-		uri=uri.open
+		#uri=uri.open
 		str=uri.read
 		return str
 	end
 
-	#gets the summoner_id
+	#initializes the champion id 
 	def summoner_id(login)
 		champion= extractJSON(login)
 		id = JSON.parse(champion)['chelski']['id']
 		@@champ_id=id
-		return id
 		
 	end 
 
+	#returns massive JSON of ranked stats
+	def ranked_stats_JSON(login)
+		summoner_id(login)
+		#url="https://"+@@region+".api.pvp.net/api/lol/"+@@region+"/v1.3/by-summoner/" + @@champ_id.to_s+"/ranked?season=SEASON2015&api_key=28ba1d65-cda8-4e79-90ad-aad6b1ab6326"
+		url="https://"+@@region+".api.pvp.net/api/lol/"+@@region+"/v1.3/stats/by-summoner/"+@@champ_id.to_s+"/ranked?season=SEASON2015&api_key=28ba1d65-cda8-4e79-90ad-aad6b1ab6326"
+		uri=URI.parse(url)
+		str=uri.read
+		return str
 
-	def summoner_avg_kills_per_game(login)
 		
 	end
 end
