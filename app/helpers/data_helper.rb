a5d906bcb581d5d1d9ab4dd1b73867deb1e512ff
Brian Bigdelle <bb586@cornell.edu>
2015-06-08 20:17:09 -0400

    background image added to the body tag of the bootstrap code

--- app/helpers/data_helper.rb
+++ app/helpers/data_helper.rb
@@ -29,15 +29,16 @@ module DataHelper
 		
 	end 
 
-	#returns massive JSON of ranked stats
+	#returns massive JSON of ranked stats BY CHAMPIONID
 	def ranked_stats_JSON(login)
 		summoner_id(login)
-		#url="https://"+@@region+".api.pvp.net/api/lol/"+@@region+"/v1.3/by-summoner/" + @@champ_id.to_s+"/ranked?season=SEASON2015&api_key=28ba1d65-cda8-4e79-90ad-aad6b1ab6326"
 		url="https://"+@@region+".api.pvp.net/api/lol/"+@@region+"/v1.3/stats/by-summoner/"+@@champ_id.to_s+"/ranked?season=SEASON2015&api_key=28ba1d65-cda8-4e79-90ad-aad6b1ab6326"
 		uri=URI.parse(url)
 		str=uri.read
 		return str
-
-		
 	end
+
+	#get every champion used by id and then store in array, iterate through each champions id and get a smaller array of the stats im interested
+	#in like kills deaths etc, return each array of data 
+	
 end
